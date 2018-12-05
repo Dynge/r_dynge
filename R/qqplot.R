5956e444f76f6d1587b1805a827af67045202c83
Dynge <you@example.com>
2018-12-05 10:33:46 +0100

    Changes for the qqplot. Added a functionality of plotting against your own X axis, aswell as tweaking the look of the plot.

--- R/qqplot.R
+++ R/qqplot.R
@@ -2,58 +2,77 @@
 #'
 #' Produces a QQ plot of your data with either normal or t-distributed quantiles.
 #'
-#' @param data The dataset only containing the data coloumns correlations between.
+#' @param Y A data vector to plot on the Y axis.
+#' @param X Additional data vector that will overwrite the normality quantiales that usually go on the X axis.
+#' Useful to compare distributions of different vectors.
 #' @param type The type of quantiles. Either "normal" or "t" distributed.
 #' @param ylabel The label for the x axis.
 #' @param xlabel The label for the y axis.
 #' @return
-#' \item{data}{A dataset containing the correlational values aswell as their respective p-values.}
+#' \item{data}{A dataset containing the vectors used for the plot.}
 #' \item{plot}{A ggplot object of the plot.}
 #' @examples
 #' data(mtcars)
 #' qqplot(mtcars$mpg)
+#'
+#' data(mtcars)
+#' qqplot(mtcars$mpg, mtcars$qsec)
 #' @import tidyverse
 #' @export
-qqplot <- function(data = NULL,
-                        type = "normal",
-                        ylabel = "Observed Values",
-                        xlabel = "Theoretical Quantiles") {
-
+qqplot <- function(Y = NULL,
+                   X = NULL,
+                   type = "normal",
+                   ylabel = "Observed Values",
+                   xlabel = "Theoretical Quantiles") {
   if ((TRUE %in% (
-    class(data) == "tbl" |
-    class(data) == "data.frame" |
-    class(data) == "tbl_df")))
-    data = unlist(data, use.names = FALSE)
-  if (!(TRUE %in% (
-    class(data) == "numeric"|
-    class(data) == "double"
+    class(Y) == "tbl" |
+    class(Y) == "data.frame" |
+    class(Y) == "tbl_df"
   )))
+    Y = unlist(Y, use.names = FALSE)
+  if (!(TRUE %in% (class(Y) == "numeric" |
+                   class(Y) == "double")))
     stop("Must enter a numeric data vector.")
 
 
-  n <- length(data)
+  n <- length(Y)
   df <- n - 1
-  sorted_data <- sort(data)
-  DataNorm = tibble( x = sorted_data, zX = ( sorted_data - mean(sorted_data) ) / sd(sorted_data) )
+  sorted_Y <- sort(Y)
 
-  QTheory = seq(1 / (n * 2), 1 - 1 / (n * 2), 1 / n)
-  if(type == "normal") q <- qnorm(QTheory, mean = 0, sd = 1)
-  else if (type == "t") q <- qt(QTheory, df)
-  DataNorm$q = q
+  if (is.null(X)) {
+    QTheory = seq(1 / (n * 2), 1 - 1 / (n * 2), 1 / n)
+    if (type == "normal") {
+      X <- qnorm(QTheory, mean = 0, sd = 1)
+    }
+    else if (type == "t") {
+      X <- qt(QTheory, df)
+    }
+    linefunc <- function() {
+      stat_function(
+        fun = function(x)
+          x * sd(Y) + mean(Y),
+        col = "red",
+        size = 1
+      )
+    }
+  } else {
+    X = sort(X)
+    linefunc <- function() {
+      geom_smooth(method = "lm",
+                  se = FALSE,
+                  col = "red")
 
+    }
+  }
 
-  qqplot <- ggplot(DataNorm, aes(q, x)) +
-    geom_point(alpha = .5, size = 3) +
-    stat_function(
-       fun = function(x)
-         x * sd(data) + mean(data),
-         col = "red",
-         size = 1
-      ) +
+  plotdata = tibble(X = X, Y = sorted_Y)
+
+  qqplot <- ggplot(plotdata, aes(X, Y)) +
+    geom_point(pch = 1, size = 3) +
+    linefunc() +
     theme_dynge() +
     xlab(xlabel) +
     ylab(ylabel)
   plot
-  return(list(data = DataNorm, plot = qqplot))
+  return(list(data = plotdata, plot = qqplot))
 }
-
