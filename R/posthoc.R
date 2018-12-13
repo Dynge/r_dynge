d8e82bba4d90903987e4baf380e0d9e7aa4d6208
Dynge <you@example.com>
2018-12-13 10:18:15 +0100

    Edited the bonferroni-holm function.

--- R/posthoc.R
+++ R/posthoc.R
@@ -1,4 +1,4 @@
-#' Bonferonni-Holm
+#' Bonferonni-Holm Correction
 #'
 #' This function computes the bonferroni-holm correction and determines if the p-values
 #' entered are significant or not.
@@ -8,6 +8,9 @@
 #' @return
 #' \item{nSig}{The number of significant results.}
 #' \item{p_val}{The significant p-values.}
+#' \item{n}{The amount of p-values corrected. The same as the length of your input vector.}
+#' \item{data}{A dataset containing the p-values, their respective adjusted alpha level,
+#' aswell as a boolean indicating if the p-value is significant or not.}
 #' @export
 bonholm_cor <- function(p_vals, alpha = .05) {
   if (!is.numeric(p_vals)) {
@@ -22,5 +25,9 @@ bonholm_cor <- function(p_vals, alpha = .05) {
   first_accept <- which(non_significant == TRUE)[1]
   significant <- sorted_pvals[seq_len(first_accept - 1)]
   return(list(nSig = length(significant),
-              p_val = significant))
+              p_val = significant,
+              n = n,
+              data = tibble(p_vals = sorted_pvals,
+                            alpha = adjusted_alpha,
+                            signi = !non_significant)))
 }
